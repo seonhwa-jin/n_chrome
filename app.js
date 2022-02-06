f0efdf812fa7b53801ecb3ed439f959b673176b9
Seonhwa <ahfbd17@naver.com>
2022-02-07 00:30:49 +0900

    22.02.06 chrome

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-alert("hi");
+
+
+// const seonhwaCalculator = {
+//   add: function( jelly , bean ){
+//     return jelly + bean ;
+//   },
+//   minus: function( jelly , bean ){
+//     return jelly - bean ;
+//   },
+//   multi: function( jelly , bean ){
+//     return jelly * bean ;
+//   },
+//   divide: function( jelly , bean){
+//      return jelly / bean ;
+//   },
+//   powerof: function ( jelly , bean){
+//     return jelly**bean ;
+//   }
+// };
+
+// const result= seonhwaCalculator.add(1,1);
+
+
+
+
+
+
+
+
+// const age = 96;
+// function calKrAge(ageOfForeigner){
+//   return ageOfForeigner + 2;
+// }
+
+// const krAge = calKrAge(age);
+
+
+
+
+
+
+
+// console.log(krAge);
+
+
+
+
+
+const age = parseInt(prompt("How old are you?"));
+
+console.log(age);
+
