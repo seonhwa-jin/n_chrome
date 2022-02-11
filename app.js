570a173e3a54c72bb5954efcc024e4159c8682ec
Seonhwa <ahfbd17@naver.com>
2022-02-11 15:50:41 +0900

    22.02.11 Chrome

--- app.js
+++ app.js
@@ -40,13 +40,45 @@
 
 
 
-// console.log(krAge);
+
+//age calculator!
+// const age = parseInt(prompt("How old are you?"));
 
 
 
+// if (isNaN(age) || age<0 ){
+//   console.log("Please write a real positive number")
+// } else if(age < 18){
+//   console.log("You are too young")
+// } else if(age >= 18 && age<=50 ){
+//   console.log("You can drink")
+// } else{
+//   console.log("You can't drink")
+// }
 
 
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(age);
+console.dir(title);
 
+function handleTitleClick(){
+  title.style.color="pink";
+}
+
+function handleMouseEnter(){
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseleave(){
+  title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click",handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseleave);
+
+function handleWindowResize(){
+  document.body.style.backgroundColor = "tomato";
+}
+
+window.addEventListener("resize",handleWindowResize);
