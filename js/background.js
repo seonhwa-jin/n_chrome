b601aebdcd4029a6a0b62b39e4dd222be391f23c
Seonhwa <ahfbd17@naver.com>
2022-03-20 22:45:56 +0900

    22.03.20 Chrome_CSS

--- js/background.js
+++ js/background.js
@@ -1,13 +1,22 @@
-const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"]
-
+const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
 const chosenImage = images[Math.floor(Math.random() * images.length)]
-
-
-
-
 const bgimage = document.createElement("img");
 
+const body = document.querySelector("body")
+
+
 bgimage.src= `img/${chosenImage}`;
-
-
 document.body.appendChild(bgimage);
+
+// document.body.style.backgroundImage = "url('img_tree.png')";
+body.style.backgroundImage ="";
+
+console.log(chosenImage)
+console.log(bgimage.src)
+
+
+function size(){
+  bgimage.classList.add("imgsize")
+}
+
+size();
